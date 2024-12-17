15ad6083a162e346dee101db0c5fbe296753afea
osama0010 <osama.eidev@gmail.com>
2024-12-17 04:24:41 +0200

    Image Controller + Upload Images Functionality

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadDto imageUploadRequest)
+        {
+            ValidateFileUpload(imageUploadRequest);
+
+            if(ModelState.IsValid)
+            {
+                // map dto back to domain model
+                var ImageDomainModel = new Image
+                {
+                    File = imageUploadRequest.File,
+                    Extension = Path.GetExtension(imageUploadRequest.File.FileName),
+                    FileSizeInBytes = imageUploadRequest.File.Length,
+                    FileName = imageUploadRequest.FileName,
+                    FileDescription = imageUploadRequest.FileDescription,
+                };
+
+                // Upload Image through User Repository methods
+                await imageRepository.Upload(ImageDomainModel);
+                return Ok(ImageDomainModel);
+
+
+            }
+
+            return BadRequest(ModelState);
+
+        }
+
+        private void ValidateFileUpload(ImageUploadDto request)
+        {
+            var AllowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+
+            if (!AllowedExtensions.Contains(Path.GetExtension(request.File.FileName)))
+            {
+                ModelState.AddModelError("File", "Not Supported Extension");
+            }
+
+            if(request.File.Length > 10485760)
+            {
+                ModelState.AddModelError("File", "Size Exceeded Maximum Allowed size (less than 10 MB)");
+
+            }
+
+
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NZWalks.API.Data;
@@ -20,6 +21,8 @@ namespace NZWalks.API
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddHttpContextAccessor();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(options =>
@@ -43,7 +46,7 @@ namespace NZWalks.API
                                 Type = ReferenceType.SecurityScheme,
                                 Id = JwtBearerDefaults.AuthenticationScheme
                             },
-                            Scheme = "Auth2",
+                            Scheme = "OAuth2",
                             Name = JwtBearerDefaults.AuthenticationScheme,
                             In = ParameterLocation.Header
                         },
@@ -66,6 +69,7 @@ namespace NZWalks.API
             builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
             builder.Services.AddScoped<IWalkRepository, WalkRepository>();
             builder.Services.AddScoped<ITokenRepository, TokenRepository>();
+            builder.Services.AddScoped<IImageRepository, LocalImageRepository>();
 
 
 
@@ -117,6 +121,12 @@ namespace NZWalks.API
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Images")),
+                RequestPath = "/Images"
+            });
 
             app.MapControllers();
 
